96f8d6eeade4f63c28b14bb3871404c533cf327a
한태현 <hth@hantaehyeon-ui-MacBookPro.local>
2025-02-07 14:07:58 +0900

    3160:solved

--- codes/3160/3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.cpp
+++ codes/3160/3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.cpp
@@ -1,5 +1,5 @@
 /*
-3160. Find the Number of Distinct Colors Among the Balls
+
 
 You are given an integer limit and a 2D array queries of size n x 2.
 
@@ -37,21 +37,84 @@ After query 1, ball 0 has color 1, and ball 1 has color 2.
 After query 2, ball 0 has color 1, and balls 1 and 2 have color 2.
 After query 3, ball 0 has color 1, balls 1 and 2 have color 2, and ball 3 has color 4.
 After query 4, ball 0 has color 1, balls 1 and 2 have color 2, ball 3 has color 4, and ball 4 has color 5.
+
+
+Constraints:
+
+1 <= limit <= 10^9
+1 <= n == queries.length <= 10^5
+queries[i].length == 2
+0 <= queries[i][0] <= limit
+1 <= queries[i][1] <= 10^9
+
 */
 
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <unordered_map>
+
 
 using namespace std;
+
+
+
+template <typename T>
+void printVector(const vector<T> vec) {
+    for (const auto& elem : vec) {
+        cout << elem << " ";
+    }
+    cout << endl;
+}
+void print(const vector<int>& ret, const vector<int>& ans)
+{
+	cout << "{";
+	for (int i = 0; i < ans.size(); ++i) {
+		assert(ret[i] == ans[i]);
+		cout << ret[i];
+		if (i != ans.size() - 1)
+			cout << ",";
+	}
+	cout << "}" << endl;
+}
 
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        
+        vector <int> res;
+        unordered_map <int,int> color_balls;
+        unordered_map <int,int> color_count;
+        for (const vector<int>& query: queries){
+            int index = query[0];
+            int color = query[1];
+            if (color_balls.find(index)!=color_balls.end())
+                if (--color_count[color_balls[index]] ==0)
+                    color_count.erase(color_balls[index]);
+            color_balls[index] = color;
+            color_count[color]++;
+            res.push_back(color_count.size());
+        }
+        return res;
     }
 };
+int main()
+{
+	Solution sol;
+	vector<vector<int>> queries;
+	vector<int> res;
+	vector<int> ans;
 
-int main(){
+	queries = {{1,4},{2,5},{1,3},{3,4}};
+	res = sol.queryResults(4, queries);
+	ans = {1,2,2,3};
+	print(res, ans);
+	
+	queries = {{0,1},{1,2},{2,2},{3,4},{4,5}};
+	ans = {1,2,2,3,4};
+	res = sol.queryResults(4, queries);
+	print(res, ans);
+
+	return 0;
 	
 }
+
