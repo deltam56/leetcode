4294332b70c2087cbb00eacd70ae43ed85c9231f
TaehyunHan <deltam56@naver.com>
2025-10-14 09:36:59 +0900

    [YS] 3349, 2247

---
+++ codes/2273/2273_Find_Resultant_Array_After_Removing_Anagrams.cpp
@@ -0,0 +1,89 @@
+/**
+* 2273. Find Resultant Array After Removing Anagrams
+* You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+* 
+* In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams, and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+* 
+* Return words after performing all operations. It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
+* 
+* An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original letters exactly once. For example, "dacb" is an anagram of "abdc".
+* 
+*  
+* 
+* Example 1:
+* 
+* Input: words = ["abba","baba","bbaa","cd","cd"]
+* Output: ["abba","cd"]
+* Explanation:
+* One of the ways we can obtain the resultant array is by using the following operations:
+* - Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index 2 and delete words[2].
+*   Now words = ["abba","baba","cd","cd"].
+* - Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index 1 and delete words[1].
+*   Now words = ["abba","cd","cd"].
+* - Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2 and delete words[2].
+*   Now words = ["abba","cd"].
+* We can no longer perform any operations, so ["abba","cd"] is the final answer.
+* Example 2:
+* 
+* Input: words = ["a","b","c","d","e"]
+* Output: ["a","b","c","d","e"]
+* Explanation:
+* No two adjacent strings in words are anagrams of each other, so no operations are performed.
+*  
+* 
+* Constraints:
+* 
+* 1 <= words.length <= 100
+* 1 <= words[i].length <= 10
+* words[i] consists of lowercase English letters.
+**/
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <print>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+		unordered_map<char, int> chk1;
+		vector<string> ret;
+		ret.push_back(words[0]);
+		for(char c : words[0])
+			chk1[c]++;
+
+		for(int i = 1; i < words.size(); ++i){
+			unordered_map<char, int> chk2;
+			for(char c : words[i]){
+				chk2[c]++;
+			}
+			if(chk1 != chk2){
+				ret.push_back(words[i]);
+			}
+			chk1 = chk2;
+		}
+		return ret;
+    }
+};
+
+int main() {
+	Solution sol;
+	vector<string> words;
+	vector<string> ans,res;
+
+	words = {"abba","baba","bbaa","cd","cd"};
+	ans = {"abba","cd"};
+	res = sol.removeAnagrams(words);
+	println("ans : {}, res : {}",ans, res);
+
+	words = {"a","b","c","d","e"};
+	ans = {"a","b","c","d","e"};
+	res = sol.removeAnagrams(words);
+	println("ans : {}, res : {}",ans, res);
+
+    return 0;
+}
